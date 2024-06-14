26c0fba9992cbceac60fedd8b1e9510a5f10c815
AmmaraGillani <ammara.gillani79@gmail.com>
2024-06-14 18:00:20 +0500

    create the about page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ const Header: React.FC = () => {
     <div className='flex justify-between items-center p-3 max-w-6xl mx-auto '>
         <div className='flex gap-4'>
             <MenuItems title = 'home' address = '/' Icon={< AiFillHome/>} />
-            <MenuItems title = 'about' address = '/' Icon={<BsFillInfoCircleFill />} />
+            <MenuItems title = 'about' address = '/about' Icon={<BsFillInfoCircleFill />} />
         </div>
         <div className='flex items-center gap-4'>
           <DarkModeSwitch/>
