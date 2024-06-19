17251c80bc7ee4ced204423032b0741aacee7556
AmmaraGillani <ammara.gillani79@gmail.com>
2024-06-19 14:03:02 +0500

    add loading effect using next js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,12 +8,14 @@ export default async function Home({ searchParams }: RequestParams) {
   const params = new URLSearchParams(searchParams); // Convert to URLSearchParams
   const genre = params.get('genre');
   const res = await fetch(
-    `https://api.themoviedb.org/3${
-    genre === 'fetchTopRated' ? '/movie/top_rated' : '/trending/all/week'
-  }?api_key=${API_KEY}&language=en-US&page=1`);
+        `https://api.themoviedb.org/3${
+        genre === 'fetchTopRated' ? '/movie/top_rated' : '/trending/all/week'
+      }?api_key=${API_KEY}&language=en-US&page=1`,
+       {next: {revalidate: 10}}
+      );
   const data = await res.json();
   if(!res.ok){
-    throw new Error ('Failed to fetch data')
+    throw new Error ('Failed to fetch data');
   }
   const results = data.results;
     return (
