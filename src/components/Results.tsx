1d93fff4b28c02290f3e53ce7b36bfd7ab37bb95
AmmaraGillani <ammara.gillani79@gmail.com>
2024-06-20 12:07:06 +0500

    Add card component

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Card from '@/components/Card';
 interface Result {
     id: number;
     original_title: string;
@@ -11,12 +12,11 @@ interface Result {
     if (!Array.isArray(results)) {
       return <div>No results available</div>;
     }  return (
-    <div>
+    <div className='sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-6xl mx-auto py-4'>
         {
-            results.map((result) => 
-            <div key={result.id}>
-                <h2>{result.original_title}</h2>
-            </div>)
+            results.map((result) => (
+              <Card key={result.id} result={result}/>
+            ))
         }
     </div>
   )
