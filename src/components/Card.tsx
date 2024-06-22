77bfe613b00528edbbe932f4be769182693527e7
AmmaraGillani <ammara.gillani79@gmail.com>
2024-06-22 17:54:54 +0500

    add movie page

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,7 +3,22 @@ import Link from 'next/link'
 import Image from 'next/image'
 import { FiThumbsUp } from 'react-icons/fi'
 
-export default function Card({result}) {
+type Result = {
+  id: number;
+  backdrop_path: string | null;
+  poster_path: string | null;
+  overview: string;
+  title: string | null;
+  name: string | null;
+  release_date: string | null;
+  first_air_date: string | null;
+  vote_count: number;
+};
+type CardProps = {
+  result: Result;
+};
+
+export default function Card({result} : CardProps) {
   return (
     <div className='group cursor-pointer sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200'>
         <Link href = {`/movie/${result.id}`}>
@@ -18,7 +33,7 @@ export default function Card({result}) {
             <div className='p-2'>
                 <p className='line-clamp-2 text-md'>{result.overview}</p>
                 <h2 className='text-lg font-bold truncate'>{result.title || result.name}</h2>
-                <p className='flex items-center'>{result.release_data || result.first_air_date}
+                <p className='flex items-center'>{result.release_date || result.first_air_date}
                 <FiThumbsUp className='h-5 mr-1 ml-3'/>
                 {result.vote_count}
                 </p>
